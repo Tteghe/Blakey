51d48c6ff09e887502fde2dbaf490d70707775b5
Tteghe <152012646+Tteghe@users.noreply.github.com>
2023-12-30 20:47:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['27673311830', 'Blake 2.0', true],
+  ['6281255369012', 'Blake', true], 
   [''] 
 ] //Number of owners
 
